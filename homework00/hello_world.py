d4b78ba1dcd6233e3bb6021eb0ad1e267ca6f5d6
Sofa-maruysh <Sofia.maruysh@mail.ru>
2025-10-02 13:50:08 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+
 def text():
-    return message
+    return "message"
+
+if __name__ == "__main__":
+    print(text())
+#

cf537c0a97a62b803993cd42787ddee814e491f2
Sofa-maruysh <Sofia.maruysh@mail.ru>
2025-10-16 14:18:15 +0300

    Реализована функция is_prime(n)

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -4,7 +4,6 @@ import typing as tp
 
 def is_prime(n: int) -> bool:
     """
-    Tests to see if a number is prime.
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -12,8 +11,12 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n <= 1:
+        return False  
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:

39170be0217bc0a0ecfd911bcc78f668379bc29f
Sofa-maruysh <Sofia.maruysh@mail.ru>
2025-10-30 12:22:07 +0300

    mypy

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -104,13 +104,16 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
 
 
 if __name__ == "__main__":
-    print("RSA Encrypter/Decrypter")
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
     print("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
-    print("Your public key is", public, "and your private key is", private)
+    print("Your public key is ", public, " and your private key is ", private)
     message = input("Enter a message to encrypt with your private key: ")
     encrypted_msg = encrypt(private, message)
-    print("Your encrypted message is:")
-    print("Decrypting message with public key", public, " . . .")
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
     print("Your message is:")
     print(decrypt(public, encrypted_msg))
